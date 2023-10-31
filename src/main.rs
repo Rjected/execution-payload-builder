c0d9b9127bcd7f676be260b1cb33198cc9cee678
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-10-30 23:07:51 -0400

    complete tx conversion

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use reth::primitives::{
     transaction::{TxEip1559, TxEip2930, TxEip4844, TxLegacy},
-    AccessList, Header as PrimitiveHeader, SealedBlock, SealedHeader,
-    Transaction as PrimitiveTransaction, TransactionSigned, Withdrawal as PrimitiveWithdrawal,
-    U256,
+    AccessList, AccessListItem, Header as PrimitiveHeader, SealedBlock, Signature,
+    Transaction as PrimitiveTransaction, TransactionKind, TransactionSigned,
+    Withdrawal as PrimitiveWithdrawal, U64,
 };
 use reth::rpc::types::Transaction;
 use reth::rpc::{
@@ -37,7 +37,7 @@ fn main() {
     // TODO: upstream into rpc compat
     let txs = match block.transactions {
         // this would be an error in upstream
-        BlockTransactions::Hashes(hashes) => {
+        BlockTransactions::Hashes(_hashes) => {
             panic!("send the eth_getBlockByHash request with full: `true`")
         }
         BlockTransactions::Full(txs) => txs,
@@ -46,12 +46,21 @@ fn main() {
     };
 
     // convert transactions into primitive transactions
-    let body: Vec<TransactionSigned> = todo!("convert txs into primitive txs");
+    let body: Vec<TransactionSigned> = txs
+        .into_iter()
+        .map(rpc_transaction_to_primitive_transaction)
+        .collect();
 
     // convert header into a primitive header
     let header = rpc_header_to_primitive_header(block.header).seal_slow();
 
-    // TODO: blob versioned hashes from txs
+    // extract blob versioned hashes from txs
+    let mut blob_versioned_hashes = Vec::new();
+    for tx in &body {
+        if let PrimitiveTransaction::Eip4844(tx) = &tx.transaction {
+            blob_versioned_hashes.push(tx.blob_versioned_hashes.clone());
+        }
+    }
 
     // convert withdrawals into primitive withdrawals
     let withdrawals: Option<Vec<PrimitiveWithdrawal>> = block.withdrawals.map(|withdrawals| {
@@ -62,7 +71,6 @@ fn main() {
     });
 
     // convert into an execution payload
-    // TODO: this will fail if transactions are not full.
     // TODO: upstream into rpc compat
     let sealed = SealedBlock {
         header,
@@ -74,6 +82,7 @@ fn main() {
     // convert to execution payload
     let execution_payload = try_block_to_payload(sealed);
 
+    println!("Execution payload: {:#?}", execution_payload);
     // convert into something that can be sent to the engine, ie `cast rpc` or something
     // this needs to be combined with the parent beacon block root, and blob versioned hashes
 }
@@ -118,63 +127,99 @@ fn rpc_withdrawal_to_primitive_withdrawal(withdrawal: Withdrawal) -> PrimitiveWi
 // convert a rpc transaction to a primitive transaction
 fn rpc_transaction_to_primitive_transaction(transaction: Transaction) -> TransactionSigned {
     let nonce = transaction.nonce.to();
-    let to = transaction.to;
+    let to = match transaction.to {
+        Some(addr) => TransactionKind::Call(addr),
+        None => TransactionKind::Create,
+    };
     let value = transaction.value.into();
     let chain_id = transaction.chain_id.unwrap().to();
     let input = transaction.input;
-    // todo: convert access list item types
-    let access_list = AccessList(transaction.access_list.unwrap_or_default());
+    let access_list = AccessList(
+        transaction
+            .access_list
+            .unwrap_or_default()
+            .into_iter()
+            .map(|item| AccessListItem {
+                address: item.address,
+                storage_keys: item.storage_keys,
+            })
+            .collect(),
+    );
     let gas_limit = transaction.gas.to();
+
+    // this is definitely a signed tx
+    let rpc_signature = transaction.signature.unwrap();
+
+    // massive chain ids can be ignored here
+    let v: u64 = rpc_signature.v.to();
+    let odd_y_parity = if v >= 35 {
+        // EIP-155: v = {0, 1} + CHAIN_ID * 2 + 35
+        ((v - 35) % 2) != 0
+    } else {
+        // non-EIP-155 legacy scheme, v = 27 for even y-parity, v = 28 for odd y-parity
+        if v != 27 && v != 28 {
+            panic!("non-eip-155 legacy v value")
+        }
+        v == 28
+    };
+
+    // convert the signature
+    let signature = Signature {
+        r: rpc_signature.r,
+        s: rpc_signature.s,
+        odd_y_parity,
+    };
+
     // just condition on tx type
-    if transaction.transaction_index == Some(U256::from(3)) {
+    let tx = if transaction.transaction_type == Some(U64::from(3)) {
         PrimitiveTransaction::Eip4844(TxEip4844 {
             chain_id,
             nonce,
             gas_limit,
             max_fee_per_gas: transaction.max_fee_per_gas.unwrap().to(),
             max_priority_fee_per_gas: transaction.max_priority_fee_per_gas.unwrap().to(),
-            to: (),
+            to,
             value,
             access_list,
             blob_versioned_hashes: transaction.blob_versioned_hashes,
             max_fee_per_blob_gas: transaction.max_fee_per_blob_gas.unwrap().to(),
             input,
         })
-    }
-    if transaction.transaction_type == Some(U256::from(2)) {
+    } else if transaction.transaction_type == Some(U64::from(2)) {
         PrimitiveTransaction::Eip1559(TxEip1559 {
             chain_id,
             nonce,
             gas_limit,
             max_fee_per_gas: transaction.max_fee_per_gas.unwrap().to(),
             max_priority_fee_per_gas: transaction.max_priority_fee_per_gas.unwrap().to(),
-            to: (),
+            to,
             value,
             access_list,
             input,
         })
-    }
-    if transaction.transaction_type == Some(U256::from(1)) {
+    } else if transaction.transaction_type == Some(U64::from(1)) {
         PrimitiveTransaction::Eip2930(TxEip2930 {
             chain_id,
             nonce,
             gas_price: transaction.gas_price.unwrap().to(),
             gas_limit,
-            to: (),
+            to,
             value,
             access_list,
             input,
         })
-    }
+    } else {
+        // otherwise legacy
+        PrimitiveTransaction::Legacy(TxLegacy {
+            chain_id: Some(chain_id),
+            nonce,
+            gas_price: transaction.gas_price.unwrap().to(),
+            gas_limit,
+            to,
+            value,
+            input,
+        })
+    };
 
-    // otherwise legacy
-    PrimitiveTransaction::Legacy(TxLegacy {
-        chain_id,
-        nonce,
-        gas_price: transaction.gas_price.unwrap().to(),
-        gas_limit,
-        to: (),
-        value,
-        input,
-    })
+    TransactionSigned::from_transaction_and_signature(tx, signature)
 }
